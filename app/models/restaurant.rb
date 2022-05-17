7fd8ff4cc261a4e981f7b98e3cf7582cf74bc1bf
Alexander Rovang <DXRovang@gmail.com>
2022-05-17 11:26:57 -0600

    fixes rating NaN, begins image addition

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,13 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   has_many :reviews
+  has_many_attached :images
+
 end
+
+# <%= form_with model: @restaurant, local: true do |form| %>
+#   <%= form.text_field :title, placeholder: "Title" %>
+#   <%= form.text_area :content %>
+#   <%= form.file_field :images, multiple: true %>
+#   <%= form.submit %>
+#   <% end %>
